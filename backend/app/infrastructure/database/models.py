1e3b516c3b9925cfc3ffa101c1a03c8a5ca8423c
Marcos André Camargo Belo <marcos.belo@dinfor.uniceub>
2026-04-28 18:08:55 -0300

    Adicionando novos arquivos e table UsuariosModel V2.1.4

--- backend/app/infrastructure/database/models.py
+++ backend/app/infrastructure/database/models.py
@@ -11,3 +11,10 @@ class TransacaoModel(Base):
     categoria = Column(String(50), nullable=False)
     descricao = Column(String(150))
     data = Column(Date, default=date.today)
+class UsuarioModel(Base):
+    __tablename__ = "cadastro"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(65),nullable=False)
+    email = Column(String(65),nullable=False, unique=True, index=True)
+    senha = Column(String(100),nullable=False)
